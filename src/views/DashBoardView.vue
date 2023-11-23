0456be86ca4d36b24c8a25dfdd5de55132b01b6e
unknown <s.m.samiulalam15@gmail.com>
2023-11-23 19:30:37 +0600

    first commit

---
+++ src/views/DashBoardView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+
+<template>
+    <div>
+        <h2>Welcome to Dashboard</h2>
+    </div>
+    <RouterView/>
+    
+</template>
